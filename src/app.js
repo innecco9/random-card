850f5ba3cff5f8a456adc7fd96042f1fe31b5218
Gabriel Innecco <innecco9@gmail.com>
2017-11-23 02:11:37 +0000

    finished card generator

--- src/app.js
+++ src/app.js
@@ -1,2 +1,30 @@
 
-import './css/styles.css';
+import './css/styles.scss';
+
+window.onload = function(){
+    
+    
+    var theSuit = document.querySelector('.suit');
+    theSuit.className = 'card suit ' + getRandomsuit();
+    
+    getRandomsuit();
+    
+}
+
+function getRandomsuit(){
+    var rand = Math.floor(Math.random()* 4)
+    switch(rand){
+        case 0:
+            return 'hearts';
+            break;
+        case 1:
+            return 'clubs'
+            break;
+        case 2:
+            return 'spades'
+            break;
+        case 3:
+            return 'diamonds'
+            break;
+    }
+}
