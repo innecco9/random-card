f1aa8e6895ea4e68cb1b54a7cd0d6646fab52044
Gabriel Innecco <innecco9@gmail.com>
2018-01-01 23:25:12 +0000

    concluded random card

--- src/app.js
+++ src/app.js
@@ -5,26 +5,28 @@ window.onload = function(){
     
     
     var theSuit = document.querySelector('.suit');
-    theSuit.className = 'card suit ' + getRandomsuit();
+    theSuit.className = 'card  suit ' + getRandomsuit();
     
     getRandomsuit();
     
-}
+    
+    
+};
 
 function getRandomsuit(){
-    var rand = Math.floor(Math.random()* 4)
+    var rand = Math.floor(Math.random()* 4);
     switch(rand){
         case 0:
             return 'hearts';
             break;
         case 1:
-            return 'clubs'
+            return 'clubs';
             break;
         case 2:
-            return 'spades'
+            return 'spades';
             break;
         case 3:
-            return 'diamonds'
+            return 'diamonds';
             break;
     }
 }
